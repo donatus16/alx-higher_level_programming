fe3a35eb0953a230750294f4171d8abb63e2105f
donatus16 <donatuskto@gmail.com>
2023-06-08 13:21:35 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import argparse
 import sys
-if len(sys.argv) == 0:
-    print("{} argument".format(len(sys.argv)))
-else:
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+elif len(sys.argv) > 2:
     i = 1
     print("{} arguments".format(len(sys.argv) - 1))
     while i <= len(sys.argv) - 1:
-        print("{} : {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
         i += 1
