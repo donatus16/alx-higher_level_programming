611560ff3602f69dd8fa1e3d003b6c34aaffa750
donatus16 <donatuskto@gmail.com>
2023-06-08 17:16:28 -0700

    103-fast_alphabet

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     SUM = 0
-    for i in range(len(sys.argv)- 1):
+    for i in range(len(sys.argv) - 1):
         SUM += int(sys.argv[i + 1])
     print("{}".format((SUM)))
