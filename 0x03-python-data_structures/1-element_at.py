9c1ae804d8c25263b21cd4cc24860e417022e243
donatus16 <donatuskto@gmail.com>
2023-06-12 01:39:49 -0700

    2-replace_in_list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = my_list[idx]
-    x = None
     if idx < 0 or idx > (len(my_list) - 1):
-        return(x)
+        return(None)
     else:
         return(i)
