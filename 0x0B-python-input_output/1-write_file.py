669ab4b26e85260c8d2279f54efa861657a4050b
donatus16 <donatuskto@gmail.com>
2023-07-12 16:02:04 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# 1-number_of_lines.py
+"""Defines a text file line-counting function."""
+
+
 def number_of_lines(filename=""):
     """Return the number of lines in a text file."""
     lines = 0
