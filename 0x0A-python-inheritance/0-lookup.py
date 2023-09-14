2dc69fb55a4150cec4d688b62533372055c6c858
donatus16 <donatuskto@gmail.com>
2023-09-14 23:56:14 +0100

    exo1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Returns a function lookup()"""
+
+
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+    """Function that returns the list of available attributes and methods,
+    f an object
+
+    Args:
+        obj (class): object
+
+    Returns:
+        list: list of available attributes and methods of an object
+    """
+    return dir(obj)
